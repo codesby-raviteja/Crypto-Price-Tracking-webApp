383b2dff532268d128fc3c13b2084a89e82bc908
codesby-raviteja <raviteja555@gmail.com>
2025-02-18 13:31:55 +0530

    add env file

--- src/Components/config.js
+++ src/Components/config.js
@@ -1,10 +1,12 @@
 // export const SingleCoin_URL = (id) =>
 //   `https://api.coingecko.com/api/v3/coins/${id}`
 
+import conf from "../conf/conf"
+
 export const TrendingCoins_URL = (currency) =>
   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`
 
-const NEWS_API_KEY = "pub_6690213b85c72fd44cda262702c4e86eda4ac"
+const NEWS_API_KEY = conf.newsApiKey
 
 export const NEWS_API = (coin) => {
   return `https://newsdata.io/api/1/latest?apikey=${NEWS_API_KEY}&qInTitle=${coin}&language=en`
@@ -14,12 +16,11 @@ export const COINS_LIST_URL = (currency) =>
   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}`
 
 export const FETCHING_OPTIONS = {
-  
   method: "GET",
   headers: {
     accept: "application/json",
-    
-    "x-cg-demo-api-key": "CG-EyKC6D1HN21HMLVy4RHDHFVR",
+
+    "x-cg-demo-api-key": conf.coinGeckoKey,
   },
 }
 
---
+++ src/conf/conf.js
@@ -0,0 +1,6 @@
+const conf = {
+  coinGeckoKey: String(import.meta.env.VITE_COINGECKO_API_KEY),
+  newsApiKey: String(import.meta.env.VITE_NEWS_API_KEY_KEY),
+}
+
+export default conf
